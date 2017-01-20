b4f265c5baaaf42c12462f831ec3f641066bff62
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-20 12:22:09 -0600

    Add Digest Excercise

---
+++ Digest/app.js
@@ -0,0 +1,37 @@
+(function(){
+    'use strict';
+
+    angular.module('CounterApp', [])
+            .controller('CounterController', CounterController);
+
+    CounterController.$inject = ['$scope'];
+    function CounterController($scope){
+        
+        $scope.onceCounter = 0;
+        $scope.counter = 0;
+
+        $scope.showNumberOfWatchers = function () {
+            console.log("# of Watchers: " + $scope.$$watchersCount);
+        };   
+
+        $scope.countOnce = function(){
+            $scope.onceCounter = 1;
+        }; 
+
+        $scope.upCounter = function(){
+            $scope.counter++;
+        };
+
+        $scope.$watch('onceCounter', function(newVal, oldVal){
+            console.log("once old value: ", oldVal);
+            console.log("once new value", newVal);
+        });
+
+        $scope.$watch('counter', function(newVal, oldVal){
+            console.log("counter old value: ", oldVal);
+            console.log("counter new value", newVal);
+        });
+
+    }
+
+})();
