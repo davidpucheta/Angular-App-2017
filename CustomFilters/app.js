1af17faffd3702355b2b03a7b639a8b0550e4f22
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-20 11:34:49 -0600

    Add CustomFilters Exercises

--- CustomFilters/app.js
+++ CustomFilters/app.js
@@ -2,22 +2,46 @@
     'use strict';
 
     angular.module('MsgApp', ['ngRoute'])
-            .controller('MsgController', MsgController);
+            .controller('MsgController', MsgController)
+            .filter('loves', LovesFilter)
+            .filter('truth', TruthFilter);
 
-    MsgController.$inject = ['$scope', '$filter'];
-    function MsgController($scope, $filter){
+    MsgController.$inject = ['$scope', '$filter', 'lovesFilter', 'truthFilter' ];
+    function MsgController($scope, $filter, lovesFilter, truthFilter){
         $scope.name = "David";
         $scope.stateOfBeing = "hungry";
         $scope.cookieCost = .45;
 
         $scope.sayMessage = function () {
-            var msg = "Eat healthy snacks at night";
+            var msg = "likes to eat healthy snacks at night";
             var output = $filter('uppercase')(msg);
             return output;
         };
 
         $scope.feedDavid = function(){
             $scope.stateOfBeing = "fed";
+        };
+
+        $scope.sayLovesMessage = function () {
+            var msg = "likes to eat healthy snacks at night";
+            msg = lovesFilter(msg);
+            return msg;
+        };
+    }
+
+    function LovesFilter() {
+        return function(input){
+            input = input || "";
+            input = input.replace("likes","loves");
+            return input;
+        };
+    }
+
+    function TruthFilter(){
+        return function(input, target, replace){
+            input = input || "";
+            input = input.replace(target,replace);
+            return input;
         };
     }
 
