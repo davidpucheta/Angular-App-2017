310b51025d484938481d072bee9b2e3c11b80751
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-23 12:24:07 -0600

    Finish Custom Services app.js

--- CustomServices/app.js
+++ CustomServices/app.js
@@ -23,6 +23,11 @@ function ShoppingListShowController(ShoppingListService) {
      var showList = this;
 
      showList.items = ShoppingListService.getItems();
+
+     showList.removeItem = function(itemIndex){
+         ShoppingListService.removeItem(itemIndex);
+     };
+
 }
 
 function ShoppingListService() {
@@ -42,6 +47,10 @@ function ShoppingListService() {
 
     service.getItems = function(){
         return items;
+    };
+
+    service.removeItem = function(itemIndex){
+        items.splice(itemIndex, 1);
     };
 }
 
